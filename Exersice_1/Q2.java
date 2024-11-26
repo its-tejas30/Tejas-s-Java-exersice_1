5753dc20daafd03fc24c0871f853a90df9c7317c
its-tejas30 <tejaspichad2002@gmail.com>
2024-11-26 11:12:49 +0530

    Add files via upload

---
+++ Exersice_1/Q2.java
@@ -0,0 +1,13 @@
+package Exersice_1;
+
+public class Q2 {
+
+	public static void main(String[] args) {
+		System.out.println("     j           A           V      V        A");
+		System.out.println("     j         A   A          V    V       A   A");
+		System.out.println(" J   J        A A A A          V  V       A A A A");
+		System.out.println("   j         A       A           V       A        A");
+
+	}
+
+}
---
+++ Exersice_1/Q3.java
@@ -0,0 +1,18 @@
+package Exersice_1;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		System.out.println("Q.3 WAP to print a table");
+		System.out.println("Ans :");
+		System.out.println("|  Name    |  Location   |  Cource      |");
+		System.out.println("-----------------------------------------");
+		System.out.println("|  Tejas   | Khadavli    |  Full stack  |");
+		System.out.println("|  Mnasi   | Sion        |  Medical     |");
+		System.out.println("|  Purva   | Thane       |  Hacker      |");
+		System.out.println("|          |             |              |");
+		System.out.println("|          |             |              |");
+		System.out.println("|          |             |              |");
+	}
+
+}
